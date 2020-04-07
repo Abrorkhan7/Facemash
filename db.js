20d0257f98021261c1b143a7f21ac1932551e783
Abror <khamidov10abror@gmail.com>
2020-04-07 13:38:34 +0500

    Setup app for Heroku

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb')
-
+const port = process.env.PORT || 3000
 const connectionString = 'mongodb+srv://abrorkhamidov:9582675aB@abrorcluster-izppc.mongodb.net/facemash?retryWrites=true&w=majority'
 //const connectionString = 'mongodb://localhost:27017/facemash'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
@@ -7,7 +7,8 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
         console.log('connected to db..');
         module.exports = client.db()
         const app = require('./app')
-        app.listen(3000)
+
+        app.listen(port)
     }else{
         console.log('error on connecting to db');
     }
